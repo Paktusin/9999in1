d78889f340db3e70ad8e87434466102d5f3e72b0
davlichin.ma <davlichin.ma@talantiuspeh.ru>
2019-02-08 18:01:23 +0300

    fix seagulls

--- src/app.js
+++ src/app.js
@@ -1,61 +1,25 @@
-import './main.scss'
-import images from './images';
-import SeaGull from './SeaGull';
+let canvas, cabasContext;
 
-let currentPicture = 9999;
-let loaded = 0;
-let seaguls = [];
-
-function nextPicture(index) {
-    if (currentPicture >= images.backs.length - 1) {
-        currentPicture = 0;
-    } else {
-        currentPicture++;
-    }
-    if (index) currentPicture = index;
-    const oldBg = $(`.bg`);
-    const newBg = $(`<div class="bg"></div>`);
-    newBg.css({backgroundImage: `url(${images.backs[currentPicture]})`});
-    smoothReplace(oldBg, newBg, () => {
-        $('#fire').toggle([10, 11].indexOf(currentPicture) !== -1);
-        seaguls.forEach(seagul => seagul.setIsNight((9 <= currentPicture && currentPicture <= 14)))
-    });
-    smoothReplace($('#canvas img'), $(`<img src="${images.backs[currentPicture]}" />`));
+window.onload = () => {
+    load();
 }
 
-function smoothReplace(oldEl, newEl, cb) {
-    newEl.css({opacity: 0});
-    newEl.insertAfter(oldEl);
-    newEl.animate({opacity: 1}, () => {
-        oldEl.remove();
-        if (typeof cb === 'function') {
-            cb()
-        }
-    });
+window.onresize = resize;
+
+function resize() {
+    canvas.height = document.body.offsetHeight;
+    canvas.width = document.body.offsetWidth;
 }
 
-function start() {
-    nextPicture();
-    setInterval(_ => {
-        nextPicture();
-    }, 3000);
-    setInterval(_ => {
-        $('#fire').toggleClass('next')
-    }, 100);
-
-    seaguls = [
-        new SeaGull($('#canvas')), new SeaGull($('#canvas'))
-    ];
+function load() {
+    canvas = document.getElementById('canvas');
+    canvasContext = canvas.getContext('2d');
+    let fps = 30;
+    resize();
+    setInterval(updateAll, 1000 / fps);
 }
 
-window.loadedBack = () => {
-    loaded++;
-    if (loaded === images.backs.length) start();
-};
-
-images.backs.forEach(image => {
-    const imageEl = $(`<img src="${image}" onload="loadedBack()"/>`);
-    $('#preload').append(imageEl)
-});
-
-
+function updateAll() {
+    canvasContext.fillStyle = '#000';
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+}
